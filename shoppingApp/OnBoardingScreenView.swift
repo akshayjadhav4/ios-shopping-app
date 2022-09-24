1822eaf076478eaa49648c3ea09496e5f4106ddc
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-09-24 17:47:33 +0530

    on boarding screen and first navigation done

--- shoppingApp/OnBoardingScreenView.swift
+++ shoppingApp/OnBoardingScreenView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct OnBoardingScreenView: View {
     var body: some View {
-        Text("On Boarding screen View")
+        NavigationView {
+            VStack(spacing:30) {
+                Spacer()
+                Image("img")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .padding()
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Order Your Favourite Fruits")
+                        .fontWeight(.bold)
+                        .font(.system(.largeTitle))
+                    Text("Eat fresh fruits and try to be healthy")
+                        .font(.system(.title3))
+                        .foregroundColor(.black)
+                        .opacity(0.7)
+                }
+                Spacer()
+                NavigationLink(destination: HomeScreenView()){
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color("Green"))
+                        .frame(width: 280, height: 60, alignment: .trailing)
+                        .overlay(
+                            HStack(alignment: .center, spacing:10) {
+                                Text("Next")
+                                    .font(.system(.title))
+                                    .fontWeight(.bold)
+                                 Image(systemName: "chevron.right")
+                            }.foregroundColor(.black)
+                        )
+                }
+                Spacer()
+            }
+        
+            .navigationBarHidden(true)
+        }
     }
 }
 
