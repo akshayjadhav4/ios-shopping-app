00e42d1a7537501e47f1ce32915f06e001f81a8f
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-09-24 19:24:05 +0530

    implemented clip shape on image

--- shoppingApp/HomeScreenViews/DetailsView.swift
+++ shoppingApp/HomeScreenViews/DetailsView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+struct RoundedCornerShape: Shape {
+    let corners: UIRectCorner
+    let radius: CGFloat
+  
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        
+        return Path(path.cgPath)
+    }
+    
+}
+
 struct DetailsView: View {
     @State var fruit: FruitTilte
     var body: some View {
-        Text("\(fruit.rawValue)")
+            VStack(alignment: .leading, spacing: 25) {
+                Image("bg").resizable()
+                    .edgesIgnoringSafeArea(.top)
+                    .clipShape(RoundedCornerShape.init(corners: [.bottomLeft,.bottomRight], radius: 50))
+                    .frame(width: .infinity, height: .infinity, alignment: .top)
+                    
+                Spacer()
+            }
+            
     }
 }
 
