eefde673e8f561c149629139a4330e73ace6c024
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-09-24 16:43:12 +0530

    Initial Commit

---
+++ shoppingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shoppingApp
+//
+//  Created by Akshay Anil Jadhav on 24/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shoppingApp/shoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingAppApp.swift
+//  shoppingApp
+//
+//  Created by Akshay Anil Jadhav on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct shoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
